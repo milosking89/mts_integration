cfff6174a4a0baadfb073783b46be4f494e0191f
milosking89 <113762091+milosking89@users.noreply.github.com>
2025-06-06 11:35:52 +0200

    Test
    Project upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using mts_integration.Application.AuthService;
+using mts_integration.Application.DataService;
+using mts_integration.RestAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<AuthService>();
+builder.Services.AddHttpClient<IApiDataProvider, ApiDataProvider>();
+builder.Services.AddSingleton<IGenerateData, GenerateData>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
